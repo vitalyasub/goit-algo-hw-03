46816ad99ff7e6eaa82a2a337fb5daee3f6c5a0d
vitalyasub <vitalya.sub1213@gmail.com>
2025-03-07 09:51:30 +0200

    change_task_02

--- 02_Get_Numbers_Ticket.py
+++ 02_Get_Numbers_Ticket.py
@@ -1,13 +1,13 @@
 import random
 
-def get_numbers_ticket(min_val, max_val, quantity):
+def get_numbers_ticket(min_val: int, max_val: int, quantity: int) -> list:
 
-    if not (1 <= min_val <= max_val <= 1000) or not (min_val <= quantity <= max_val):
+    if min_val < 1 or max_val > 1000 or quantity < 1 or quantity > (max_val - min_val + 1):
         return []
     
     numbers = random.sample(range(min_val, max_val + 1), quantity)
     return sorted(numbers)
 
 # Приклад використання:
-lottery_numbers = get_numbers_ticket(1, 64, 10)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
